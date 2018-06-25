3795e8d431dd4b35caa4814065e1cd88c3a399ee
Ray Kim <msca8h@naver.com>
2018-06-26 00:46:33 +0900

    finished initial implementation

--- HW3/linalg.h
+++ HW3/linalg.h
@@ -12,10 +12,28 @@
 
 extern "C"
 {
-    void dgesvd_(char *jobu, char *jobvt, int *m, int *n,
-                 double *A, int *lda, double *S, double *U,
-                 int *ldu, double *VT, int *ldvt, double* work,
-                 int *lwork, int *info);
+    void dgesvd_(char *jobu,
+                 char *jobvt,
+                 int *m, int *n,
+                 double *A, int *lda,
+                 double *S,
+                 double *U, int *ldu,
+                 double *VT, int *ldvt,
+                 double* work, int *lwork, int *info);
+
+    void dgemm_(char* TRANSA, char* TRANSB,
+                int* M, int* N, int* K,
+                double* ALPHA, double* A, int* 	LDA,
+                double* B, int* LDB, double* BETA,
+                double* C, int* LDC);	
+
+    void dgemv_(char* TRANSA,
+                int* M, int* N, 
+                double* ALPHA, double const* A, int* LDA,
+                double const* X, int* INCX, double* BETA,
+                double* Y, int* INCY);	
+
+    double dnrm2_(int* n, double const* x, int* incx);
 }
 
 namespace linalg
@@ -27,6 +45,11 @@ namespace linalg
         double* _data;
 
     public:
+        inline
+        dense_matrix()
+            :_m(0), _n(0), _data(nullptr)
+        {}
+
         inline
         dense_matrix(size_t m, size_t n)
             : _m(m),
@@ -58,6 +81,35 @@ namespace linalg
             other._n = 0;
         }
 
+        inline dense_matrix&
+        operator=(dense_matrix const& other) noexcept
+        {
+            if(_data != nullptr)
+                free(_data);
+
+            _m = other._m;
+            _n = other._n;
+            _data = static_cast<double*>(
+                malloc(_m * _n * sizeof(double)));
+            memcpy(_data, other._data, _m * _n * sizeof(double));
+            return *this;
+        }
+
+        inline dense_matrix&
+        operator=(dense_matrix&& other) noexcept
+        {
+            if(_data != nullptr)
+                free(_data);
+
+            _data = other._data;
+            _m = other._m;
+            _n = other._n;
+            other._data = nullptr;
+            other._m = 0;
+            other._n = 0;
+            return *this;
+        }
+
         inline void
         normalize_cols()
         {
@@ -71,6 +123,14 @@ namespace linalg
                                [total, eps](double elem)
                                {return elem / (total + eps); } );
             }
+        }
+
+        inline double
+        row_norm(size_t i)
+        {
+            int n = _n;
+            int offset = _m;
+            return dnrm2_(&n, &_data[i], &offset);
         }
 
         inline double 
@@ -100,22 +160,112 @@ namespace linalg
             if(_data != nullptr)
                 free(_data);
         }
+
     };
 
     inline std::tuple<dense_matrix, std::vector<double>, dense_matrix>
-    svd(dense_matrix const& mat, size_t order_k)
+    svd(dense_matrix const& mat)
     {
-        size_t m = mat.shape().first;
-        size_t n = mat.shape().second;
-        assert(order_k <= mat.shape().second);
+        int m = mat.shape().first;
+        int n = mat.shape().second;
         auto U = dense_matrix(m, m);
-        auto Vt = dense_matrix(order_k, order_k);
-        auto D = std::vector<double>(order_k);
+        auto Vt = dense_matrix(n, n);
+        auto D = std::vector<double>(n);
+        int lwork = std::max(3 * std::min(m,n) + std::max(m,n),
+                             5 * std::min(m,n));
+        auto work = std::vector<double>(lwork);
+        int info = 0;
 
         char job = 'a';
-        dgesvd_(&job, &job, &m, &n, mat.data(), int *lda, double *S, double *U,
-                int *ldu, double *VT, int *ldvt, double* work,
-                int *lwork, int *info);
+        dgesvd_(&job, &job, &m, &n, mat.data(), &m, D.data(), U.data(),
+                &m, Vt.data(), &n, work.data(), &lwork, &info);
+        return {U, D, Vt};
+    }
+
+    inline std::vector<double>
+    operator*(double a, std::vector<double> const& v)
+    {
+        std::vector<double> result(v);
+        std::transform(v.begin(), v.end(), result.begin(),
+                       [a](double elem){ return a * elem; });
+        return result;
+    }
+
+    inline dense_matrix
+    operator*(dense_matrix const& A, dense_matrix const& B)
+    {
+        assert(A.shape().second == B.shape().first);
+        char t = 'n';
+        double alpha = 1;
+        double beta = 0;
+        int m = A.shape().first;
+        int k = A.shape().second;
+        int n = B.shape().first;
+        auto C = dense_matrix(m, n);
+
+        dgemm_(&t, &t,
+               &m, &n, &k,
+               &alpha, A.data(), &m,
+               B.data(), &k, &beta,
+               C.data(), &m);
+        return C;
+    }
+
+    inline void
+    print_matrix(char const* label, dense_matrix const& mat) {
+        int m = mat.shape().first;
+        int n = mat.shape().second;
+
+        int i, j;
+        printf("%s\n",label);
+        for (i=0; i<m; i++){
+            for (j=0; j<n; j++) 
+                printf("%20.10lf ", mat(i,j));
+            printf("\n");
+        }
+        printf("\n");
+    }
+
+    inline dense_matrix
+    approx_matscaling(dense_matrix const& U,
+                      std::vector<double> const& D,
+                      size_t approx_rank_k)
+    {
+        size_t m = U.shape().first;
+        auto C = dense_matrix(m, approx_rank_k);
+
+        for(size_t j = 0; j < approx_rank_k; ++j) {
+            for(size_t i = 0; i < m; ++i)
+                C(i, j) = U(i, j) * D[j];
+        }
+        return C;
+    }
+
+    inline double
+    norm(std::vector<double> const& vec)
+    {
+        int n = vec.size();
+        int offset = 1;
+        return dnrm2_(&n, vec.data(), &offset);
+    }
+
+    inline std::vector<double>
+    gemv(double alpha,
+         dense_matrix const& A,
+         std::vector<double> const& v)
+    {
+        std::vector<double> result(v.size());
+        char t = 'N';
+        int m = A.shape().first;
+        int n = A.shape().second;
+        double beta = 0;
+        int incx = 1;
+        int incy = 1;
+        dgemv_(&t, &m, &n, 
+               &alpha, A.data(), &m,
+               v.data(), &incx, &beta,
+               result.data(), &incy);	
+        return result;
     }
 }
 
--- HW3/main.cpp
+++ HW3/main.cpp
@@ -1,16 +1,27 @@
 
+#include <chrono>
 #include <exception>
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <limits>
 #include <optional>
 #include <string>
+#include <sstream>
 #include <vector>
 
 #include "linalg.h"
 
 namespace hw3
 {
+    inline decltype(auto)
+    time_elapsed(std::chrono::time_point<std::chrono::steady_clock> begin,
+                 std::chrono::time_point<std::chrono::steady_clock> end)
+    {
+        return std::chrono::duration_cast<
+            std::chrono::milliseconds>(end - begin);
+    }
+
     using namespace std::string_literals;
     class word_corpus
     {
@@ -52,7 +63,7 @@ namespace hw3
         }
 
         inline std::optional<size_t>
-        operator[](std::string const& key)
+        operator[](std::string const& key) const
         {
             auto elem = std::lower_bound(_words.begin(), _words.end(), key);
             if(elem == _words.end() || key != *elem)
@@ -68,9 +79,10 @@ namespace hw3
         }
     };
 
-    class retrieval_accuracy
+    class optimizer
     {
-        inline retrieval_accuracy(std::string const& path)
+    public:
+        inline optimizer(std::string const& path)
         {
             std::ifstream word_stream;
             word_stream.open(path + "/documentkey.txt"s);
@@ -82,26 +94,25 @@ namespace hw3
     template<typename Matrix>
     class document_database
     {
-    public:
+    private:
         word_corpus _words;
-        retrieval_accuracy _accuracy;
+        optimizer _accuracy;
+        Matrix _latent_space_matrix;
         Matrix _doc_matrix;
+        Matrix _encoder_matrix;
 
         size_t count = 0;
 
         inline std::vector<std::string>
-        format_string(std::string const& str)
+        split_string(std::string const& str) const
         {
             auto _str = str;
-            std::transform(str.begin(), str.end(), _str.begin(), ::tolower);
             auto splitted = std::vector<std::string>(1, _str);
 
             if(_str.back() == 's')
                 splitted.emplace_back(_str.begin(), _str.end() - 1);
 
-            if(std::all_of(_str.begin(),_str.end(), ::isalnum))
-                return splitted;
-            else
+            if(!std::all_of(_str.begin(),_str.end(), ::isalnum))
             {
                 auto begin = _str.begin();
                 auto end = _str.end();
@@ -114,30 +125,92 @@ namespace hw3
                     curr = std::find_if_not(prev, end, ::isalnum);
                 }
                 splitted.emplace_back(prev, curr);
-                return splitted;
             }
+            return splitted;
         }
 
         inline void
-        parse_document(std::ifstream& file_stream, size_t doc_idx)
+        parse_document(std::ifstream& file_stream, size_t doc_idx, bool pred)
         {
-            std::string word;
-            while(file_stream >> word)
-            {
-                auto tokens = format_string(word);
+            std::vector<std::string> words;
+            words.reserve(1000);
+            std::copy(std::istream_iterator<std::string>(file_stream),
+                      std::istream_iterator<std::string>(), std::back_inserter(words));
 
-                std::for_each(
-                    tokens.begin(), tokens.end(),
-                    [&](std::string const& word)
+            if(words.size() == 0)
+                return;
+
+            std::transform(words.begin(), words.end(), words.begin(),
+                           [](std::string const& word){
+                               std::string result(word);
+                               std::transform(word.begin(), word.end(), result.begin(), ::tolower);
+                               return result;
+                           });
+
+            auto register_word =
+                [&](std::string word){
+                    auto word_entry = _words[word];
+                    if(word_entry)
                     {
-                        auto word_entry = _words[word];
-                        if(word_entry)
-                        {
-                            ++_doc_matrix(doc_idx, word_entry.value());
-                            ++count;
-                        }
-                    });
+                        ++_doc_matrix(doc_idx, word_entry.value());
+                        ++count;
+                    }
+                };
+
+            for(auto it = words.begin(); it != words.end(); ++it)
+            {
+                auto tokens = split_string(*it);
+                std::for_each(tokens.begin(), tokens.end(), register_word);
             }
+
+            for(auto it = words.begin(); std::next(it) != words.end(); ++it)
+            {
+                register_word(*it + *std::next(it));
+                register_word(*it + '_' + *std::next(it));
+                register_word(*it + '-' + *std::next(it));
+            }
+        }
+
+        inline std::vector<double>
+        compute_similarity(std::vector<double> const& word_vec) const
+        {
+            auto query_norm = linalg::norm(word_vec);
+            auto temp = gemv(1.0, _encoder_matrix, word_vec);
+            return gemv((1 / query_norm), _latent_space_matrix, temp);
+        }
+
+        inline std::vector<size_t>
+        topk_similar_documents(std::vector<double> const& similarity, size_t k) const
+        {
+            auto highest_elem_indices = std::vector<size_t>();
+            highest_elem_indices.reserve(k + 1);
+
+            for(size_t i = 0; i < k; ++i)
+                highest_elem_indices.push_back(i);
+
+            std::sort(highest_elem_indices.begin(),
+                      highest_elem_indices.end(),
+                      [&similarity](size_t first, size_t second)
+                      { return similarity[first] > similarity[second]; });
+
+            for(size_t i = k; i < similarity.size(); ++i)
+            {
+                if(similarity[i] > similarity[highest_elem_indices.back()])
+                {
+                    auto it = std::upper_bound(
+                        highest_elem_indices.begin(),
+                        highest_elem_indices.end(),
+                        similarity[i],
+                        [&similarity](double val, size_t second)
+                        { return val >= similarity[second]; });
+
+                    auto begin = highest_elem_indices.begin();
+                    auto dist = it - begin ;
+                    highest_elem_indices.insert(begin + dist, i);
+                    highest_elem_indices.pop_back();
+                }
+            }
+            return highest_elem_indices;
         }
 
     public:
@@ -147,70 +220,193 @@ namespace hw3
                           size_t end_idx)
             : _words(path),
               _accuracy(path),
-              _doc_matrix(end_idx - begin_idx, _words.size())
+              _doc_matrix(end_idx - begin_idx, _words.size()),
+              _encoder_matrix()
         {
             auto placeholder = std::string();
             auto file_name = path + "/doc000.txt"s;
+            auto big_file_name = path + "/doc0000.txt"s;
 
             std::cout << "-- constructing document matrix\n";
-            auto begin = &file_name[file_name.size() - 7];
+            auto start = std::chrono::steady_clock::now();
             for(size_t i = begin_idx; i < end_idx; ++i)
             {
                 std::ifstream stream;
                 placeholder = std::to_string(10000 + i);
-                std::copy(&placeholder[2], &placeholder[5], begin);
-                stream.open(file_name);
-                parse_document(stream, i);
+                if(i < 1000)
+                {
+                    auto begin = &file_name[file_name.size() - 7];
+                    std::copy(&placeholder[2], &placeholder[5], begin);
+                    stream.open(file_name);
+                }
+                else
+                {
+                    auto begin = &big_file_name[big_file_name.size() - 8];
+                    std::copy(&placeholder[1], &placeholder[5], begin);
+                    stream.open(big_file_name);
+                }
+
+                if(stream)
+                    parse_document(stream, i, false);
             }
+            auto stop = std::chrono::steady_clock::now();
 
             std::cout << "-- constructing document matrix - done\n";
             std::cout << "   processed " << end_idx - begin_idx << " documents"
-                      << ", counted " << count << " words"
+                      << ", counted " << count << " words\n"
+                      << "   time elapsed is " << time_elapsed(start, stop).count() << "ms"
+                      << std::endl;
+            _doc_matrix.normalize_cols();
+        }
+
+        inline std::vector<std::pair<size_t, double>>
+        query(std::vector<std::string> terms,
+              size_t num_result) const
+        {
+            std::vector<double> word_vec(_words.size(), 0);
+            for(auto const& i : terms)
+            {
+                auto entry = _words[i];
+                if(!entry)
+                    throw std::runtime_error('\'' + i + "\' not found in database.");
+                word_vec[entry.value()] = 1;
+            }
+            auto sim_vec = compute_similarity(word_vec);
+            auto top_k_documents = topk_similar_documents(sim_vec, num_result);
+
+            auto results = std::vector<std::pair<size_t, double>>(num_result);
+            for(size_t i = 0; i < num_result; ++i)
+            {
+                size_t doc_idx  = top_k_documents[i];
+                results[i].first = doc_idx;
+                results[i].second = sim_vec[doc_idx];
+            }
+            return results; 
+        }
+
+        inline void
+        decompose(size_t k)
+        {
+            std::cout << "-- computing low rank approximation \n";
+            auto start = std::chrono::steady_clock::now();
+
+            auto [U, D, Vt] = svd(_doc_matrix);       
+            _latent_space_matrix = approx_matscaling(U, D, k);
+            _encoder_matrix = std::move(Vt);
+
+            auto norms = std::vector<double>(_latent_space_matrix.shape().first);
+            for(size_t i = 0; i < _doc_matrix.shape().first; ++i)
+                norms[i] = _latent_space_matrix.row_norm(i);
+
+            for(size_t j = 0; j < _latent_space_matrix.shape().second; ++j) {
+                for(size_t i = 0; i < _latent_space_matrix.shape().first; ++i)
+                    _doc_matrix(i, j) /= norms[i];
+            }
+            auto stop = std::chrono::steady_clock::now();
+            std::cout << "-- computing low rank approximation - done\n"
+                      << "   time elapsed is " << time_elapsed(start, stop).count() << "ms"
+                      << std::endl;
+        }
+    };
+
+    class terminal
+    {
+    private:
+        document_database<linalg::dense_matrix> _db;
+
+    public:
+        explicit terminal(std::string const& path)
+            : _db(path, 0, 1012)
+        {
+            size_t default_k = 500;
+            std::cout << "-- using default approximation rank\n"
+                      << "   default rank is " << default_k
+                      << std::endl;
+            _db.decompose(default_k);
+        }
+
+        void
+        query(std::string const& query) const
+        {
+            std::string term;
+            std::stringstream stream(query);
+            std::vector<std::string> terms{};
+            while(stream >> term)
+                terms.push_back(std::move(term));
+
+            auto result = std::vector<std::pair<size_t, double>>();
+            auto start = std::chrono::steady_clock::now();
+            try
+            { result = _db.query(terms,10); }
+            catch(std::runtime_error const& e)
+            {
+                std::cout << " Error: " << e.what() << std::endl;
+                return;
+            }
+            auto stop = std::chrono::steady_clock::now();
+
+
+            std::cout << "-----   query result   -----\n\n"
+                      << "     document  cosine \n"
+                      << "     index     similarity\n"
+                      << std::endl;
+            for(size_t i = 0; i < result.size(); ++i)
+            {
+                printf("%3d %4d       %f\n",
+                       (int)i+1, (int)result[i].first, result[i].second);
+            }
+            std::cout << "\n time elapsed is "
+                      << time_elapsed(start, stop).count() << "ms\n"
                       << std::endl;
         }
     };
 }
 
-
 int main()
 {
     std::cout << "****  CSEG347 HW3: Document Search Engine  ****" << std::endl;
     std::string const path = "database";
-    auto db = hw3::document_database<linalg::dense_matrix>(path, 0, 219);
+    hw3::terminal _term(path);
 
     while(true)
     {
-        std::string word;
+        std::string input;
         std::cout<< "$ ";
-        std::cin >> word;
-        auto result = db._words[word];
-        if(!result)
-            std::cout << "not found" << std::endl;
-        else
-        {
-            auto& mat = db._doc_matrix;
-            size_t idx = result.value();
-            size_t count = std::accumulate(&mat(0, idx),
-                                        &mat(0, idx + 1),
-                                        0.0f);
-            std::cout << "index: " << idx << " count: " << count << std::endl;
-        }
+        std::getline(std::cin, input);
+        if(input.empty())
+            continue;
+        if(input[0] != ':')
+            _term.query(input);
     }
+    // auto& mat = db._doc_matrix;
+    // auto& corpus = db._words;
+    // for(size_t i = 0; i < mat.shape().second; ++i)
+    // {
+    //     double count = std::accumulate(&mat(0, i),
+    //                                    &mat(0, i + 1),
+    //                                    0.0);
+    //     if(count == 0)
+    //     {
+    //         std::cout << "found no occurance of: \"" << corpus[i]
+    //                   << "\" index: " << i << std::endl;
+    //     }
+    // }
 
     // while(true)
     // {
+    //     std::string word;
     //     std::cout<< "$ ";
-    //     size_t i, j
-    //     std::cin >> i;
-    //     std::cin >> j;
+    //     std::cin >> word;
     //     auto result = db._words[word];
     //     if(!result)
     //         std::cout << "not found" << std::endl;
     //     else
     //     {
-    //         auto& mat = db._doc_matrix;
-    //         std::cout << " val: " << mat(i, j) << std::endl;
+    //         size_t idx = result.value();
+    //         size_t count = std::accumulate(&mat(0, idx),
+    //                                        &mat(0, idx + 1),
+    //                                        0.0f);
+    //         std::cout << "index: " << idx << " count: " << count << std::endl;
     //     }
     // }
-
 }
